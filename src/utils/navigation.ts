15b97d2adfd5ba26c1b0bc5d19fffbb695b547c2
OUSSAMA ERROUJI <107694414+ErroujiOussama@users.noreply.github.com>
2024-08-27 15:37:33 +0100

    change logos

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,21 +1,16 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  { name: "Products", url: "/products" },
+  { name: "Scholarships", url: "/blog" },
+  { name: "Internship Opportunities", url: "/Internship Opportunities"},
+  { name: "PhD Opportunities", url: "/PhD Opportunities" },
+  { name: "Job Opportunities", url: "/blog" },
+  { name: "Contact", url: "/contact" },
   { name: "Services", url: "/services" },
   { name: "Blog", url: "/blog" },
-  { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
-  {
-    section: "Ecosystem",
-    links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
-    ],
-  },
   {
     section: "Company",
     links: [
